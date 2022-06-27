333d35ea1508865d1cba7987687f0a71abd7b70b
Peter Sun <petersun@Peters-MacBook-Air.local>
2022-06-28 08:51:20 +1200

    internal2

--- FlaskUserDemo/app.py
+++ FlaskUserDemo/app.py
@@ -6,12 +6,23 @@ app = Flask(__name__)
 from utils import create_connection, setup
 app.register_blueprint(setup)
 
-# my first home page which is my login page
+@app.before_request
+def restrict():
+    restricted_pages = [
+        'list_users',
+        'view_user',
+        'edit_user',
+        'delete_user'
+    ]
+    if 'logged_in' not in session and request.endpoint in restricted_pages:
+        flash("You must be logged in to view this page.")
+        return redirect('/login')
+
 @app.route('/')
 def home():
     return render_template("login.html")
 
-# backend for my login page
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -21,7 +32,7 @@ def login():
 
         with create_connection() as connection:
             with connection.cursor() as cursor:
-                sql = "SELECT * FROM user_infor WHERE email=%s AND password=%s"
+                sql = "SELECT * FROM users WHERE email=%s AND password=%s"
                 values = (
                     request.form['email'],
                     encrypted_password
@@ -32,13 +43,145 @@ def login():
             session['logged_in'] = True
             session['first_name'] = result['first_name']
             session['role'] = result['role']
-            session['user_id'] = result['user_id']
+            session['id'] = result['id']
             return redirect("/dashboard")
         else:
             flash("Invalid username or password.")
             return redirect("/login")
     else:
         return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
+@app.route('/register', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+
+        password = request.form['password']
+        encrypted_password = hashlib.sha256(password.encode()).hexdigest()
+
+        with create_connection() as connection:
+            with connection.cursor() as cursor:
+                sql = """INSERT INTO users
+                    (first_name, last_name, email, password, avatar)
+                    VALUES (%s, %s, %s, %s, %s)
+                """
+                values = (
+                    request.form['first_name'],
+                    request.form['last_name'],
+                    request.form['email'],
+                    encrypted_password,
+                )
+                cursor.execute(sql, values)
+                connection.commit()
+        return redirect('/')
+    return render_template('users_add.html')
+
+@app.route('/dashboard')
+def list_users():
+    if session['role'] != 'admin':
+        flash("Only admin can access this page.")
+        return redirect('/')
+    with create_connection() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM users")
+            result = cursor.fetchall()
+    return render_template('users_list.html', result=result)
+
+@app.route('/view')
+def view_user():
+    with create_connection() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM users WHERE id=%s", request.args['id'])
+            result = cursor.fetchone()
+    return render_template('users_view.html', result=result)
+
+@app.route('/delete')
+def delete_user():
+    with create_connection() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute("DELETE FROM users WHERE id=%s", request.args['id'])
+            connection.commit()
+    return redirect('/dashboard')
+
+@app.route('/edit', methods=['GET', 'POST'])
+def edit_user():
+    # Admin are allowed, users with the right id are allowed, everyone else sees 404.
+    if session['role'] != 'admin' and str(session['id']) != request.args['id']:
+        flash("You don't have permission to edit this user.")
+        return redirect('/view?id=' + request.args['id'])
+
+    if request.method == 'POST':
+        if request.files['avatar'].filename:
+            avatar_image = request.files["avatar"]
+            ext = os.path.splitext(avatar_image.filename)[1]
+            avatar_filename = str(uuid.uuid4())[:8] + ext
+            avatar_image.save("static/images/" + avatar_filename)
+            if request.form['old_avatar'] != 'None':
+                os.remove("static/images/" + request.form['old_avatar'])
+        elif request.form['old_avatar'] != 'None':
+            avatar_filename = request.form['old_avatar']
+        else:
+            avatar_filename = None
+
+        with create_connection() as connection:
+            with connection.cursor() as cursor:
+                sql = """UPDATE users SET
+                    first_name = %s,
+                    last_name = %s,
+                    email = %s,
+                    avatar = %s
+                WHERE id = %s"""
+                values = (
+                    request.form['first_name'],
+                    request.form['last_name'],
+                    request.form['email'],
+                    avatar_filename,
+                    request.form['id']
+                )
+                cursor.execute(sql, values)
+                connection.commit()
+        return redirect('/view?id=' + request.form['id'])
+    else:
+        with create_connection() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM users WHERE id = %s", request.args['id'])
+                result = cursor.fetchone()
+        return render_template('users_edit.html', result=result)
+
+
+# for admin to edit the subject page add and delete and all that 
+@app.route('/subjects')
+def list_subjects():
+    if session['role'] != 'admin':
+        flash("Only admin can access this page.")
+        return redirect('/')
+    with create_connection() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM users")
+            result = cursor.fetchall()
+    return render_template('users_list.html', result=result)
+
+
+@app.route('/view')
+def view_subject_infor():
+    with create_connection() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM subject_infor WHERE id=%s", request.args['subject_id'])
+            result = cursor.fetchone()
+    return render_template('subject_view.html', result=result)
+
+@app.route('/delete')
+def delete_subject_infor():
+    with create_connection() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute("DELETE FROM subject_infor WHERE id=%s", request.args['subject_id'])
+            connection.commit()
+    return redirect('/dashboard')
+
 
 
 if __name__ == '__main__':
@@ -53,5 +196,4 @@ if __name__ == '__main__':
     except ValueError:
         PORT = 5555
     app.run(HOST, PORT, debug=True)
-
 
